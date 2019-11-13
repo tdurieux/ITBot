944f50decc431525061c0393290d5b25f844364d
Javier Cabrera <javierca@kth.se>
2019-11-13 16:44:40 +0100

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,73 @@
 import * as fs from 'fs'
 import * as path from 'path';
 import Container from './core/container';
+import * as WebSocket from 'ws';
+import * as req from 'request';
 
-var program = require('commander');
- 
-function absolute(dir){
-    return path.join(process.cwd(), dir);
-}
+// To open chrome as child process
+const {  spawn } = require('child_process');
 
-
-program
-  .version('0.0.1', '-v', '--version')
-  .usage('[options] <project path>')
-  //.option('-m --minumum <minimum>', 'Minimum tree size to translate')
-  .parse(process.argv);
- 
-
-if(!program.target)
-    throw new Error("Target script is required. -t <target>")
-
-if(!program.coverage)
-    throw new Error("Target script is required. -c <coverage>")
-
-if(!program.workload)
-    throw new Error("Target script is required. -w <workload>")
+const port = process.env.port || 9222;
+const waitFor = parseInt(process.env.sessionTime) || 1000 ;// milliseconds
+const timeout = (60 + waitFor)*1000; //10 seconds
+const chromeAlias = process.env.chrome || 'chrome'
 
 
+const chrome = spawn(chromeAlias,[
+  '--remote-debugging-port='+port,
+  '--user-data-dir=temp',
+  "google.com"
+]);
 
+chrome.stdout.on('data', function(data) {
+  console.log(data.toString());
+});
+
+let interval = setTimeout(()=>{
+  chrome.kill()
+  clearTimeout(interval)
+}, timeout)
+
+// Asking for opened tabs
+
+let interval2 = setTimeout(() => {
+
+  const log = fs.openSync("log.txt", 'a')
+
+  // Accessing chrome publish websocket address
+  req(`http://localhost:${port}/json`,function (error, response, body) {
+      
+    const tab = JSON.parse(body)[0];
+  
+    console.log("Enabled websockets for tab 0")
+    console.log(tab.id, tab.url)
+  
+    const url = tab.webSocketDebuggerUrl;
+  
+    const ws = new WebSocket(url);
+  
+    ws.on('open', function open() {
+        console.log("Websocket channel opened. Enabling runtime namespace")
+         ws.send(JSON.stringify({id: 3, method: 'Runtime.enable'}))
+    });
+   
+  
+    ws.on('message', function incoming(data) {
+      
+      fs.writeSync(log, data + "\n")
+      const obj = JSON.parse(data)
+      
+      if(obj.id === 3){
+
+        // TODO Execute pipeline
+
+        
+      }
+      
+    });
+  
+  });
+  
+
+    clearTimeout(interval2)
+}, waitFor)
